86e4c1e97ade2fbc00e86fc28cd8bfb964827bd8
trizest <josh_mutch@hotmail.com>
2021-08-27 08:00:29 +0000

    process working with train test outputs

--- 2_process.py
+++ 2_process.py
@@ -9,13 +9,10 @@ exchange = 'Binance'
 pair = 'btc_usdt'
 timeframe = '15m'
 oclhvColumns = ['Unix', 'Open', 'Close', 'Low', 'High', 'Volume']
-lookback_period = [5,10,20,50,100,200,500,1000,2000]
-#variance_factor this gets put in the df and looked for along with the polynomials. 
-entry_thrsh = 1.05
-exit_thrsh = 1.10
-stoploss_thrsh = 1.05
 train_start = "2018-01-01 00:00:00"
-train_finish = "2020-0-01 00:00:00"
+train_finish = "2020-12-31 23:45:00"
+test_start = "2021-01-01 00:00:00"
+test_finish = "2021-06-30 00:00:00"
 
 ###WHOLE OF DF FUNCTIONS GO HERE. 
 #takes the csv, sets index with datetimeformat, returns a pandas dataframe
@@ -33,21 +30,28 @@ def simplify_df (d_f):
 
 
 #puts the data in the jar
-def pickle_df(ex, p, tf, d_f):
+def pickle_df(ex, p, tf, d_f,id):
     path = Path("./data/2_processed/", str(ex))
     path.mkdir(parents=True, exist_ok=True)
-    full_path = path / str(f'{p}_{tf}.pkl')
+    full_path = path / str(f'{p}_{tf}_{id}.pkl')
     d_f.to_pickle(full_path)
     print(full_path)
 
+#this fuction made redundand by simply putting the 
 def train_set(d_f):
     df_x = d_f[train_start:train_finish] 
+    return df_x
 
+#function run. saves two processed df's in pickles. 
 if __name__ == '__main__':
     df = read_csv(exchange, pair, timeframe)
     df = simplify_df (df)
-    pickle_df(exchange, pair, timeframe, df)
-    
+    train_df = df[train_start:train_finish]
+    pickle_df(exchange, pair, timeframe, train_df, "train")
+    test_df = df[test_start:test_finish]
+    pickle_df(exchange, pair, timeframe, test_df, "test")
 
-    df.info()
-    print(df.head(10))
+    train_df.info()
+    print(test_df.head(10))
+    print(test_df.tail(10))
+
